b91c5e579ae8ad2e206e76784fe2889892059425
LeleOi <leleoioioi@gmail.com>
2020-01-13 17:52:01 +0100

    Implemented Routing for multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/Movies'
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/Movies";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/NotFound";
+import NavBar from './components/NavBar'
+import MovieForm from './components/MovieForm'
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-    <Movies />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path='/movies/:id' component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
